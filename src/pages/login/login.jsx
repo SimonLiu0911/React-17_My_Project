ffa0f727349594835669da115a3092ff26a9dc2d
Simon <simon.liu@cloud-interactive.com>
2022-04-21 14:54:35 +0800

    temp

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import { Form, Input, Button } from 'antd';
 
 import "./login.scss";
 
 class Login extends Component {
+
+  handleSubmit = () => {
+    console.log('trigger handleSubmit');
+  }
   render() {
     return (
       <div className="login-content">
         <h2 className="font-size-1">用戶登錄</h2>
+        {/* <div className="form-content"> */}
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form.Item>
+              <Input
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        {/* </div> */}
       </div>
     );
   }
