02b0d10173e1246d3f988b0989b2bee087c110a5
Simon <simon.liu@cloud-interactive.com>
2022-04-25 16:54:57 +0800

    temp

--- src/components/Layout/sideNav/sideNav.jsx
+++ src/components/Layout/sideNav/sideNav.jsx
@@ -1,11 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import "./sideNav.scss";
+const sideNavList = [
+  {
+    id: "1",
+    name: "列表一",
+    path: "/list1"
+  },
+  {
+    id: "2",
+    name: "列表二",
+    path: "/list2"
+  },
+  {
+    id: "3",
+    name: "列表三",
+    path: "/list3"
+  },
+]
 
 const Sidenav = () => {
   return (
     <div id="sideNav">
       <div className="title">
         <span>後台管理</span>
+      </div>
+      <div className="sideNav">
+        <ul>
+          {
+            sideNavList.map(sideNavItem => {
+              return (
+                <li key={sideNavItem.id}>
+                  <Link to={sideNavItem.path}>{sideNavItem.name}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     </div>
   );
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from "react-router"
+
 import Layout from '../../components/Layout/layout'
+import Home from '../home/home';
+import Product from '../product/product'
+import Role from '../role/role'
 
 class Admin extends Component {
   render() {
     return (
       <Layout>
-        admin
+        <Switch>
+          <Route exact path="/list1">
+            <Home />
+          </Route>
+          <Route path="/list2">
+            <Product />
+          </Route>
+          <Route path="/list3">
+            <Role />
+          </Route>
+          <Redirect to="/list1"></Redirect>
+        </Switch>
       </Layout>
     );
   }
